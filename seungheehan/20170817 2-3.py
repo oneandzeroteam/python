e38c228ee9fe2b2c7ed422821e845d7b32a4e12b
seungheehann <heeya121hs@gmail.com>
2017-08-17 16:45:17 +0900

    index

---
+++ seungheehan/20170817 2-3.py
@@ -0,0 +1,13 @@
+
+list = [17, 92, 18, 33, 58, 7, 33, 43]
+
+maximum = 0
+for i in list:
+    if maximum <= i:
+        maximum = i
+    else:
+        maximum = maximum
+print("The biggest number in the list is %i."%maximum)
+
+index_max = list.index(maximum)
+print("Location : %i."%(index_max + 1))
