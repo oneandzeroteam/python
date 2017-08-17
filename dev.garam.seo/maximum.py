f4db753758d975a4c98eda4ae75b12b4bb1e77b7
GaramSeo <dev.garam.seo@gmail.com>
2017-08-17 16:53:13 +0900

    add maximum_index

--- dev.garam.seo/maximum.py
+++ dev.garam.seo/maximum.py
@@ -3,11 +3,16 @@
 list = [17, 92, 18, 33 ,58, 7, 33, 43]
 
 maximum = 0 
+index = 0
+j = 0
 for i in list :
 	if i>=maximum:
 		maximum = i
+		index = j  
+	j = j + 1
 
 print (maximum)
+print (index)
 
 
 
